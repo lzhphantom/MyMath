9f4c4ee829b21186a8f39dd7a1ee379ab664bad9
ratiki <987090402@qq.com>
2019-08-09 17:15:45 +0900

    v1.0.3

--- models/admin.go
+++ models/admin.go
@@ -18,7 +18,7 @@ func init() {
 	//设置数据库基本信息
 	orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/mymath?charset=utf8")
 	//映射model数据
-	orm.RegisterModel(new(Admin), new(BasicCommon), new(BasicContent), new(KnowledgeImportant), new(Formula), new(ExaminationCenter), new(HDifficulty), new(User), new(UserInfo))
+	orm.RegisterModel(new(Admin), new(BasicCommon), new(BasicContent), new(KnowledgeImportant), new(Formula), new(ExaminationCenter), new(HDifficulty), new(User), new(UserInfo), new(Question))
 	//生成表
 	orm.RunSyncdb("default", false, true)
 

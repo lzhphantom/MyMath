59a1fe4ac53ac2a6145bf102ecf1164e2a8398c8
ratiki <987090402@qq.com>
2019-08-12 19:06:18 +0900

    v1.0.3

--- models/user.go
+++ models/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	Id       int
-	UserName string    //登录用户名
+	UserName string    `orm:"unique"` //登录用户名
 	Password string    //密码
 	Role     byte      `orm:"default(1)"` //角色
 	Created  time.Time `orm:"auto_now_add;type(datetime)"`
@@ -16,8 +16,8 @@ type UserInfo struct {
 	Id      int
 	Name    string    //姓名
 	Sex     byte      //性别
-	Tel     string    `orm:"size(11);null"` //电话
-	Address string    `orm:"null"`          //地址
+	Tel     string    `orm:"size(11);null;unique"` //电话
+	Address string    `orm:"null"`                 //地址
 	Created time.Time `orm:"auto_now_add;type(datetime)"`
 	Updated time.Time `orm:"auto_now;type(datetime)"`
 	User    *User     `orm:"rel(one)"`

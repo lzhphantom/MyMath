f4f20d4b00732c9228586c2cf7a40c5752189ce3
ratiki <987090402@qq.com>
2019-08-15 17:33:27 +0900

    v1.0.3

--- static/js/userManage.js
+++ static/js/userManage.js
@@ -9,19 +9,6 @@ $(() => {
     }
     $("input,select").not("[type=submit]").jqBootstrapValidation();
 
-    $("#userAddBtn").on("click", () => {
-        let userName = $("input[name=userName]").val();
-        let password = $("input[name=password]").val();
-        let name = $("input[name=name]").val();
-        let sex = $("input[name=sex]").val();
-        let tel = $("input[name=tel]").val();
-        let province = $("select[name=province]").val();
-        let city = $("select[name=city]").val();
-        let street = $("select[name=street]").val();
-        let userGroup = $("select[name=userGroup]").val();
-        console.log(userName, password, name, sex, tel, province, city, street, userGroup);
-    });
-
 });
 
 function backToUserModal(obj) {
